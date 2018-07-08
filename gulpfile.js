4f2ca22e3b835816f8103153971d9478eb0ddf1b
Kyrylo Grebennyk <kyrylo06051986@gmail.com>
2018-07-09 00:20:02 +0300

    gulpfile modify

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,10 @@
-  // const gulp = require('gulp');
- // const imagemin = require('gulp-imagemin');
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
  
- // gulp.task('imagemin', () =>
-    // gulp.src('projects/lamzaki/images/content/photo/*')
-       //  .pipe(imagemin())
-       // .pipe(gulp.dest('dist/lamzaki/images'))
-// );
+	gulp.task('imagemin', () =>
+	gulp.src('projects/lamzaki/images/content/photo/*')
+       .pipe(imagemin())
+       .pipe(gulp.dest('dist/lamzaki/images'))
+	);
 
-   // html to pdf
-
-var pdf = require('html-pdf');
-pdf.create(html).toFile([filepath, ]function(err, res){
-  console.log(res.filename);
-});
-
-pdf.create(html).toStream(function(err, stream){
-  stream.pipe(fs.createWriteStream('./foo.pdf'));
-});
-
-pdf.create(html).toBuffer(function(err, buffer){
-  console.log('This is a buffer:', Buffer.isBuffer(buffer));
-});
-
-
-// for backwards compatibility
-// alias to pdf.create(html[, options]).toBuffer(callback)
-pdf.create(html [, options], function(err, buffer){});
+   
