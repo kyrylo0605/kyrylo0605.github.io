e66ace777cda7093a9c633adea729512a3250bdb
Kyrylo Grebennyk <kyrylo06051986@gmail.com>
2018-07-15 18:57:36 +0300

    edit gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,9 @@ const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
  
 	gulp.task('imagemin', () =>
-	gulp.src('projects/lamzaki/images/content/photo/*')
+	gulp.src('dist/before/*')
        .pipe(imagemin())
-       .pipe(gulp.dest('dist/lamzaki/images'))
+       .pipe(gulp.dest('dist/after'))
 	);
 
    
